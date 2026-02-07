63ba013390bafc3424d113fb740961686451d561
Ilnyr2006-droid <Ilnyr2006@icloud.com>
2026-02-07 23:12:26 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,349 @@
+#!/bin/bash
+# Скрипт автоматизации Модуля 1 (Полная версия)
+# Поддерживаемые роли: hq-srv, br-srv, hq-rtr, br-rtr, isp, hq-cli, hq-sw
+
+ROLE=$1
+DOMAIN="au-team.irpo"
+
+if [ -z "$ROLE" ]; then
+    echo "Использование: ./setup.sh [hq-srv|br-srv|hq-rtr|br-rtr|isp|hq-cli|hq-sw]"
+    exit 1
+fi
+
+echo "=== НАЧИНАЮ НАСТРОЙКУ РОЛИ: $ROLE ==="
+
+# --- 1. Имя хоста и время ---
+hostnamectl set-hostname "${ROLE}.${DOMAIN}"
+timedatectl set-timezone Europe/Moscow
+
+# --- 2. Настройка пользователей ---
+setup_users() {
+    echo "Настройка пользователей..."
+    if [[ "$ROLE" == *"srv"* ]]; then
+        adduser --gecos "" remote_user --disabled-password
+        adduser --uid 2026 --gecos "" sshuser --disabled-password
+        echo "sshuser:P@ssword" | chpasswd
+        usermod -aG sudo sshuser
+        echo 'sshuser ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/sshuser
+        chmod 0440 /etc/sudoers.d/sshuser
+    fi
+    if [[ "$ROLE" == *"rtr"* ]]; then
+        adduser --gecos "" net_admin --disabled-password
+        echo "net_admin:P@ssw0rd" | chpasswd
+        usermod -aG sudo net_admin
+        echo 'net_admin ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/net_admin
+        chmod 0440 /etc/sudoers.d/net_admin
+    fi
+    if [[ "$ROLE" == "hq-cli" ]]; then
+         echo "root:P@ssword" | chpasswd
+    fi
+}
+
+# --- 3. Настройка SSH ---
+setup_ssh() {
+    echo "Настройка SSH..."
+    apt update && apt install -y openssh-server
+    echo "Authorized access only" > /etc/issue.net
+    sed -i 's/#Port 22/Port 2026/' /etc/ssh/sshd_config
+    sed -i 's/#Banner none/Banner \/etc\/issue.net/' /etc/ssh/sshd_config
+    sed -i 's/#MaxAuthTries 6/MaxAuthTries 2/' /etc/ssh/sshd_config
+    
+    if [[ "$ROLE" == *"srv"* ]]; then
+        echo "AllowUsers sshuser" >> /etc/ssh/sshd_config
+    elif [[ "$ROLE" == *"rtr"* ]]; then
+        echo "AllowUsers net_admin" >> /etc/ssh/sshd_config
+    fi
+    systemctl restart ssh
+}
+
+# --- 4. Логика по ролям ---
+case $ROLE in
+    "hq-srv")
+        setup_users
+        setup_ssh
+        cat <<EOF > /etc/network/interfaces
+auto ens33
+iface ens33 inet static
+    address 192.168.10.2/27
+    gateway 192.168.10.1
+EOF
+        echo "Установка и настройка Bind9..."
+        apt install -y bind9
+        cat <<EOF > /etc/bind/named.conf.options
+options {
+    directory "/var/cache/bind";
+    forwarders { 8.8.8.8; };
+    recursion yes;
+    allow-query { any; };
+    listen-on { any; };
+    allow-recursion { any; };
+};
+EOF
+        cat <<EOF > /etc/bind/named.conf.local
+zone "au-team.irpo" { type master; file "/etc/bind/zones/db.au-team.irpo"; };
+zone "10.168.192.in-addr.arpa" { type master; file "/etc/bind/zones/db.10.168.192"; };
+zone "20.168.192.in-addr.arpa" { type master; file "/etc/bind/zones/db.20.168.192"; };
+zone "1.16.172.in-addr.arpa" { type master; file "/etc/bind/zones/db.1.16.172"; };
+zone "2.16.172.in-addr.arpa" { type master; file "/etc/bind/zones/db.2.16.172"; };
+zone "100.168.192.in-addr.arpa" { type master; file "/etc/bind/zones/db.100.168.192"; };
+EOF
+        mkdir -p /etc/bind/zones
+        cat <<EOF > /etc/bind/zones/db.au-team.irpo
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+hq-srv IN A 192.168.10.2
+hq-rtr IN A 172.16.1.2
+br-rtr IN A 172.16.2.2
+br-srv IN A 192.168.100.2
+hq-cli IN A 192.168.20.10
+docker IN A 172.16.1.1
+web    IN A 172.16.2.1
+EOF
+        cat <<EOF > /etc/bind/zones/db.10.168.192
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+2 IN PTR hq-srv.au-team.irpo.
+EOF
+        cat <<EOF > /etc/bind/zones/db.20.168.192
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+10 IN PTR hq-cli.au-team.irpo.
+EOF
+        cat <<EOF > /etc/bind/zones/db.1.16.172
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+1 IN PTR docker.au-team.irpo.
+2 IN PTR hq-rtr.au-team.irpo.
+EOF
+        cat <<EOF > /etc/bind/zones/db.2.16.172
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+1 IN PTR web.au-team.irpo.
+2 IN PTR br-rtr.au-team.irpo.
+EOF
+        cat <<EOF > /etc/bind/zones/db.100.168.192
+\$TTL 604800
+@ IN SOA hq-srv.au-team.irpo. root.au-team.irpo. ( 2026020201 604800 86400 2419200 604800 )
+@ IN NS hq-srv.au-team.irpo.
+2 IN PTR br-srv.au-team.irpo.
+EOF
+        systemctl restart bind9
+        ;;
+
+    "br-srv")
+        setup_users
+        setup_ssh
+        cat <<EOF > /etc/network/interfaces
+auto ens33
+iface ens33 inet static
+    address 192.168.100.2/27
+    gateway 192.168.100.1
+EOF
+        ;;
+
+    "hq-rtr")
+        setup_users
+        setup_ssh
+        echo "ip_gre" >> /etc/modules
+        modprobe ip_gre
+        
+        cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+auto ens33
+iface ens33 inet static
+    address 172.16.1.2/28
+    gateway 172.16.1.1
+auto ens37
+iface ens37 inet manual
+auto ens37.100
+iface ens37.100 inet static
+    address 192.168.10.1/27
+    vlan_raw_device ens37
+auto ens37.200
+iface ens37.200 inet static
+    address 192.168.20.1/28
+    vlan_raw_device ens37
+auto ens37.999
+iface ens37.999 inet static
+    address 192.168.20.1/29
+    vlan_raw_device ens37
+auto gre30
+iface gre30 inet tunnel
+    address 10.0.0.1
+    netmask 255.255.255.252
+    mode gre
+    local 172.16.1.2
+    endpoint 172.16.2.2
+    ttl 255
+    mtu 1476
+    post-up ip route replace 192.168.100.0/28 via 10.0.0.2
+EOF
+        echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+        sysctl -p
+        apt install -y iptables-persistent
+        iptables -t nat -A POSTROUTING -o ens37.100 -j MASQUERADE
+        iptables-save > /etc/iptables/rules.v4
+
+        apt install -y isc-dhcp-server
+        sed -i 's/INTERFACESv4=""/INTERFACESv4="ens37.200"/' /etc/default/isc-dhcp-server
+        cat <<EOF > /etc/dhcp/dhcpd.conf
+default-lease-time 600;
+max-lease-time 7200;
+authoritative;
+subnet 192.168.20.0 netmask 255.255.255.240 {
+    range 192.168.20.2 192.168.20.14;
+    option routers 192.168.20.1;
+    option domain-name "au-team.irpo";
+    option domain-name-servers 192.168.10.2;
+}
+EOF
+        systemctl restart isc-dhcp-server
+
+        apt install -y frr
+        sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+        systemctl restart frr
+        cat <<EOF > /etc/frr/frr.conf
+frr version 8.1
+frr defaults traditional
+hostname hq-rtr
+interface gre30
+ ip ospf authentication message-digest
+ ip ospf message-digest-key 1 md5 1c+rYtGm
+!
+router ospf
+ network 192.168.100.0/27 area 0
+ network 10.0.0.0/30 area 0
+!
+line vty
+EOF
+        systemctl restart frr
+        ;;
+
+    "br-rtr")
+        setup_users
+        setup_ssh
+        echo "ip_gre" >> /etc/modules
+        modprobe ip_gre
+
+        cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+auto ens33
+iface ens33 inet static
+    address 172.16.2.2/28
+    gateway 172.16.2.1
+auto ens37
+iface ens37 inet static
+    address 192.168.100.1/27
+auto gre30
+iface gre30 inet tunnel
+    address 10.0.0.2
+    netmask 255.255.255.252
+    mode gre
+    local 172.16.2.2
+    endpoint 172.16.1.2
+    ttl 255
+    mtu 1476
+    post-up ip route replace 192.168.10.0/28 via 10.0.0.1
+    post-up ip route replace 192.168.20.0/28 via 10.0.0.1
+EOF
+        echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+        sysctl -p
+        apt install -y iptables-persistent
+        iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+        iptables-save > /etc/iptables/rules.v4
+
+        apt install -y frr
+        sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+        systemctl restart frr
+        cat <<EOF > /etc/frr/frr.conf
+frr version 8.1
+frr defaults traditional
+hostname br-rtr
+interface gre30
+ ip ospf authentication message-digest
+ ip ospf message-digest-key 1 md5 1c+rYtGm
+!
+router ospf
+ network 192.168.0.0/28 area 0
+ network 10.0.0.0/30 area 0
+!
+line vty
+EOF
+        systemctl restart frr
+        ;;
+
+    "isp")
+        cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+auto ens33
+iface ens33 inet dhcp
+auto ens37
+iface ens37 inet static
+    address 172.16.1.1/28
+auto ens38
+iface ens38 inet static
+    address 172.16.2.1/28
+EOF
+        echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+        sysctl -p
+        apt install -y iptables-persistent
+        iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+        iptables-save > /etc/iptables/rules.v4
+        
+        apt install -y chrony
+        echo "server 0.debian.pool.ntp.org iburst" > /etc/chrony/chrony.conf
+        echo "local stratum 5" >> /etc/chrony/chrony.conf
+        echo "allow 172.16.0.0/12" >> /etc/chrony/chrony.conf
+        echo "allow 192.168.0.0/16" >> /etc/chrony/chrony.conf
+        systemctl restart chrony
+        ;;
+
+    "hq-cli")
+        cat <<EOF > /etc/network/interfaces
+auto ens33
+iface ens33 inet dhcp
+EOF
+        cat <<EOF > /etc/resolv.conf
+search au.team.irpo
+domain au.team.irpo
+nameserver 192.168.10.2
+EOF
+        ;;
+
+    "hq-sw")
+        apt install -y openvswitch-switch
+        ovs-vsctl del-br br-hq 2>/dev/null
+        ovs-vsctl add-br br-hq
+        ovs-vsctl add-port br-hq ens33 trunks=100,200,999
+        ovs-vsctl add-port br-hq ens37 tag=100
+        ovs-vsctl add-port br-hq ens38 tag=200
+        ovs-vsctl add-port br-hq mgmt0 tag=999 -- set interface mgmt0 type=internal
+        cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+allow-hotplug ens33
+iface ens33 inet manual
+allow-hotplug ens37
+iface ens37 inet manual
+allow-hotplug ens38
+iface ens38 inet manual
+auto mgmt0
+iface mgmt0 inet static
+    address 192.168.250.2/29
+    ovs_bridge br-hq
+    ovs_type OVSIntPort
+    ovs_options tag=999
+EOF
+        ;;
+esac
+
+systemctl restart networking
+echo "--- НАСТРОЙКА ЗАВЕРШЕНА. ПРОВЕРЬТЕ IP И СЛУЖБЫ ---"
